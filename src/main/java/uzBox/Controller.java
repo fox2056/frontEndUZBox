355230738cc4f9d161595d3d7817ec146495880b
fox2056 <sliepov@icloud.com>
2023-03-24 15:19:34 +0100

    Zmienione główne okno aplikacji oraz dodane okno Ustawienia konta użytkownika.

--- src/main/java/uzBox/Controller.java
+++ src/main/java/uzBox/Controller.java
@@ -3,9 +3,14 @@ package uzBox;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -42,6 +47,21 @@ public class Controller {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML
+    public void ustawieniaWindow(ActionEvent event) throws IOException {
+
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("Ustawienia");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Ustawienia.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 600);
+
+        window.setScene(scene);
+
+        window.showAndWait();
     }
 
 
